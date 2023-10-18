431ed9736f4e6a14fe306e0808da15f52e946026
mattia0419 <pironemattia6@gmail.com>
2023-10-18 15:43:40 +0200

    made controller,model and migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
-
+use App\Http\Controllers\ComicsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+Route::resource('comics', ComicsController::class);
