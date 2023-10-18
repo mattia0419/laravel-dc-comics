0d2595998eeb4535edea8c6776ce4c358866985c
mattia0419 <pironemattia6@gmail.com>
2023-10-18 22:08:47 +0200

    add seeder

--- database/migrations/2023_10_18_133417_create_comics_table.php
+++ database/migrations/2023_10_18_133417_create_comics_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
-            $table->string('thumb');
-            $table->float('price');
-
+            $table->string('title', 50);
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price', 8, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
